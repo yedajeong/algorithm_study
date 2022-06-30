b39dbc5814d4f1b0cc0155ea8bb66ff6494f191b
yedajeong <49023751+yedajeong@users.noreply.github.com>
2022-07-01 01:49:10 +0900

    입출력 #10951 풀이 (예다정)

---
+++ 입출력/10951_ydj.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+
+    while(1){
+        cin >> A >> B;
+        
+        if (!cin.eof())
+            cout << A+B << endl;
+        else
+            break;
+    }
+
+    return 0;
+}

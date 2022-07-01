d749e5fb11ebaf6fccfee540b050862f80fa73ba
yedajeong <49023751+yedajeong@users.noreply.github.com>
2022-07-01 20:30:12 +0900

    입출력 #10953 풀이 (예다정)

---
+++ 입출력/10953_ydj.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int T, split;
+    string input, A, B;
+
+    cin >> T;
+
+    for (int i=0; i<T; i++){
+        cin >> input;
+        split = input.find(',');
+        int A = stoi(input.substr(0, split));
+        int B = stoi(input.substr(split+1));
+        cout << A+B << endl;
+    }
+
+    return 0;
+}

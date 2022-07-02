0d8a4745c1a8adc0914d6fa162ac585f628ae65e
yedajeong <49023751+yedajeong@users.noreply.github.com>
2022-07-02 21:25:05 +0900

    입출력 #2442 풀이 (예다정)

---
+++ 입출력/2442_ydj.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+
+    cin >> N;
+
+    for (int i=1; i<=N; i++){
+        for (int j=0; j<N-i; j++)
+            cout << " ";
+        for (int k=0; k<2*i-1; k++)
+            cout << "*";
+
+        cout << "\n";
+    }
+
+    return 0;
+}

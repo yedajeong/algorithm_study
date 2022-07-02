b45fcc023d061de539bc7f6f1579d02a17f40828
yedajeong <49023751+yedajeong@users.noreply.github.com>
2022-07-02 20:25:12 +0900

    입출력 #8393 풀이 (예다정)

---
+++ 입출력/8393_ydj.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, result=0;
+
+    cin >> N;
+
+    for (int i=1; i<=N; i++)
+        result += i;
+
+    cout << result << "\n";
+
+    return 0;
+}

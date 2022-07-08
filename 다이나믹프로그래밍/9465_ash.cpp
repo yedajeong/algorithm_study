fe9751bf3de6be0fe4cd1e0c26f81670bb9b0db2
seheeAn <49268298+seheeAn@users.noreply.github.com>
2022-07-09 00:59:49 +0900

    스티커

---
+++ 다이나믹프로그래밍/9465_ash.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n[201] = { 0 };
+	int trynum;
+	cin >> trynum;
+
+	for (int i = 0; i < trynum; i++) { //전체 반복 횟수
+		int size;
+		cin >> size;
+		int num[200001] = { 0 };
+		for (int i = 1; i < 2 * size; i += 2) { 
+			cin >> num[i]; //첫째줄
+		}
+		for (int i = 2; i < 2 * size + 1; i += 2) {
+			cin >> num[i]; //둘째줄
+		}
+
+		n[1] = num[1];
+		n[2] = num[2];
+		n[3] = num[2] + num[3];
+		n[4] = num[1] + num[4];
+
+		if (size > 2) {
+			for (int i = 3; i < size + 1; i++) {
+				n[2 * i - 1] = max(n[2 * i - 2] + num[2 * i - 1], n[2 * (i - 2)] + num[2 * i - 1]); //윗줄로 도착
+				n[2 * i] = max(n[2 * i - 3] + num[2 * i], n[2 * i - 5] + num[2 * i]);  //아래줄로 도착
+			}
+		}
+		int maximum = max(n[2 * size], n[2 * size - 1]);
+		cout << maximum << endl;
+	}
+	return 0;
+}

83810a6d2b370aa9db79c19b85d7e132ca782d1b
yedajeong <49023751+yedajeong@users.noreply.github.com>
2022-07-18 17:56:01 +0900

    #11651 좌표 정렬하기2 (예다정)

---
+++ 자료구조+정렬+스택+.../11651_ydj.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Point{
+public:
+    int x, y;
+    Point(int _x, int _y):x(_x), y(_y){}
+};
+
+bool compare(Point a, Point b){
+    if (a.y == b.y)
+        return a.x < b.x;
+    else
+        return a.y < b.y;
+}
+
+int main(){
+    int N, x, y;
+
+    cin >> N;
+
+    vector<Point> v;
+    for (int i=0; i<N; i++){
+        cin >> x >> y;
+        v.push_back(Point(x, y));
+    }
+    
+    sort(v.begin(), v.end(), compare); // 사용자 정의 함수로 sorting
+
+    for (int i=0; i<N; i++)
+        cout << v[i].x << " " << v[i].y << '\n';
+
+    //cout << v.size() << endl;
+    return 0;
+}

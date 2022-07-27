a4cb067b78bb8ef5932cf241cfb5a535d6b16aa9
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2022-07-27 18:40:58 +0900

    Create 10809_sangjin.cpp

---
+++ 자료구조+정렬+스택+.../10809_sangjin.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string word;
+	
+	int count_alpha[27] = { 0, };
+	
+	for (int i = 0; i < 26; i++) {
+		count_alpha[i] = -1;
+	}
+	
+	cin >> word;
+	int length = word.size();
+
+	for (int i = 0; i < length; i++) {
+		if (count_alpha[word[i] - 97] == -1) {
+			count_alpha[word[i] - 97] = i;
+		}
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << count_alpha[i] << " ";
+	}
+}

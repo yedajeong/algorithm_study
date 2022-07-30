2c9add65e708e51317d8446191181dbbef43b3e6
seheeAn <49268298+seheeAn@users.noreply.github.com>
2022-07-30 16:58:39 +0900

    ROT13

---
+++ 자료구조+정렬+스택+.../11655_ash.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string input;
+
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	
+	getline(cin, input);
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] > 64 && input[i] < 91) { //대문자일 때
+			if (input[i] + 13 < 91) {
+				input[i] = char(input[i] + 13);
+			}
+			else {
+				input[i] = char(input[i] + 13 - 26);
+			}
+		}
+		else if (input[i] > 96 && input[i] < 123) { //소문자일때
+			if (input[i] + 13 < 123) {
+				input[i] = char(input[i] + 13);
+			}
+			else {
+				input[i] = char(input[i] + 13 - 26);
+			}
+		}
+	}
+	
+	cout << input << '\n';
+
+	return 0;
+}

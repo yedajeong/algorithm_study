9277bc2eaa77e603a10863306a5add6295cb0e95
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2023-01-10 00:00:42 +0900

    Update 1931_sangjin.cpp

--- 그리디알고리즘/1931_sangjin.cpp
+++ 그리디알고리즘/1931_sangjin.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include<algorithm>
-#include<vector>
+#include <vector>;
+#include <algorithm>
 using namespace std;
 
+bool cmp(const pair<int, int>& a, const pair<int, int>& b)
+{
+	return a.second < b.second;
+}
+
+
 int main() {
-
-	int N, start, end;
+	vector<pair<int, int>> conf;
+	int N;
 	cin >> N;
-	vector<pair<int, int>> schedule;
-
-	for (int i = 0; i < N; i++)
-	{
+	for (int i = 0; i < N; i++) {
+		int start, end;
 		cin >> start >> end;
-		schedule.push_back(make_pair(end, start));
+		conf.push_back(make_pair(start, end));
 	}
+	
+	sort(conf.begin(), conf.end(), cmp);
+	//그냥 sort는 정수를 자료형으로 갖는 pair의 first를 1차, second를 2차로 오름차순 정렬한다.
 
-	sort(schedule.begin(), schedule.end());
-
-	int time = schedule[0].first;
+	int end_time = conf[0].second;
 	int count = 1;
-	for (int i = 1; i < N; i++)
-	{
-		if (time <= schedule[i].second)
+	for (int i = 1; i < N; i++) {
+		if (end_time <= conf[i].first)
 		{
 			count++;
-			time = schedule[i].first;
+			end_time = conf[i].second;
 		}
 	}
 

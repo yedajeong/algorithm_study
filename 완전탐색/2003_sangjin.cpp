68d3f94b8f8536016daede941601a9698d1c57bb
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2023-02-08 14:38:06 +0900

    Update 2003_sangjin.cpp

--- 완전탐색/2003_sangjin.cpp
+++ 완전탐색/2003_sangjin.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int num[10001] = { 0 };
 
+//이건 내 풀이인데 시간복잡도 생각하면 옳지 않다.
 int main() {
 	int N, M;
 	cin >> N >> M;
@@ -32,3 +33,30 @@ int main() {
 	}
 	cout << cnt << endl;
 }
+
+/*Two-Pointer를 통해 푼 풀이
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N, M, arr[10000];
+	cin >> N >> M;
+	for(int i=0; i<N; i++){
+		cin >> arr[i];
+	}
+
+	int answer = 0, sum = 0, s = 0, e = 0;
+	while(s<N){
+		if(sum >= M){
+			sum -= arr[s];
+			s++;
+		}else{
+			sum += arr[e];
+			e++;
+		}
+		if(sum == M) answer++;
+	}
+
+	cout << answer;
+}
+*/

8850bcc97d9e02669e78b54cb5cf834e202f7d27
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2023-02-14 20:36:35 +0900

    Update and rename 7453_sangjin.cpp to 2143_sangjin.cpp

--- 완전탐색/2143_sangjin.cpp
+++ 완전탐색/2143_sangjin.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 long long int A[1001] = { 0 };
 long long int B[1001] = { 0 };
-long long int C[1001] = { 0 };
-long long int D[1001] = { 0 };
 vector<long long int>sum_a;
 vector<long long int>sum_b;
 
@@ -43,13 +41,50 @@ int main() {
 
 	sort(sum_a.begin(), sum_a.end());
 	sort(sum_b.begin(), sum_b.end());
+
+	long long int cnt = 0;
+	int start = 0;
+	int end = sum_b.size() - 1;
+
+	while (start < sum_a.size() && end >= 0) {
+		long long int a_start = sum_a[start];
+		long long int b_end = sum_b[end];
+		long long int a_cnt = 0;
+		long long int b_cnt = 0;
+
+		if (a_start + b_end > T) {
+			end--;
+		}
+		else if (a_start + b_end == T) {
+			while (start < sum_a.size() && sum_a[start] == a_start) {
+				start++;
+				a_cnt++;
+			}
+			while (end >= 0  && sum_b[end] == b_end) {
+				end--;
+				b_cnt++;
+			}
+			cnt += a_cnt * b_cnt;
+		}
+		else {
+			start++;
+		}
+	}
+
+
+	cout << cnt << endl;
+	/*
 	long long result = 0;
 
 	for (int i = 0; i < sum_b.size(); i++) {
 		int low = lower_bound(sum_a.begin(), sum_a.end(), T - sum_b[i]) - sum_a.begin();
 		int high = upper_bound(sum_a.begin(), sum_a.end(), T - sum_b[i]) - sum_a.begin();
+		
 		result += high - low;
 	}
 
 	cout << result << endl;
+	*/
+
+
 }
